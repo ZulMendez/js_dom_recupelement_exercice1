cfe1f338099c38b373e21d8596c85860a23406ac
zulma <zumendez7@gmail.com>
2021-01-26 11:10:09 +0100

    exo terminé

---
+++ main.js
@@ -0,0 +1,23 @@
+// 1
+let titres = document.getElementsByTagName('h1');
+console.log(titres);
+
+// 2
+let sousTitre = document.getElementsByTagName('h3');
+console.log(sousTitre);
+let sousT1 = sousTitre[0];
+console.log(sousT1);
+let sousT2 = sousTitre[1];
+console.log(sousT2);
+let sousT3 = sousTitre[2];
+console.log(sousT3);
+
+// 3
+let paragra = document.getElementsByTagName('p');
+let paragra2 = paragra[1];
+console.log(paragra2);
+
+// 4
+let liste = document.getElementsByTagName('li');
+let liste2 = liste[2];
+console.log(liste2);
